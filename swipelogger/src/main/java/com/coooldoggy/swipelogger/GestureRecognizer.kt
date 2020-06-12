d2c496e9ac0a8866d2927a72670bd6891a9f5150
coooldoggy <coooldoggy@gmail.com>
2020-06-12 13:41:42 +0900

    GestureDetector

--- swipelogger/src/main/java/com/coooldoggy/swipelogger/GestureRecognizer.kt
+++ swipelogger/src/main/java/com/coooldoggy/swipelogger/GestureRecognizer.kt
@@ -5,6 +5,7 @@ import android.graphics.PointF
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import androidx.lifecycle.Lifecycle
 import java.lang.ref.WeakReference
 import java.util.*
 import kotlin.collections.ArrayList
@@ -17,9 +18,35 @@ abstract class GestureRecognizer (context: Context): OnGestureRecognizerStateCha
 
     protected val currentLocation = PointF()
     protected val prevLocation = PointF()
+    protected val downLocation = PointF()
     protected var downTime: Long = 0L
     protected var prevdownTime: Long = 0L
-    protected val gestureRecognizerHandler: GestureHandler(Looper.getMainLooper())
+    protected val gestureRecognizerHandler: GestureHandler = GestureHandler(Looper.getMainLooper())
+
+    var actionListener: ((GestureRecognizer) -> Any?)? = null
+    var stateChangeListener : ((GestureRecognizer, State? ,State?) -> Unit)? = null
+
+    val downLocationX: Float
+    get() = downLocation.x
+
+    val downLocationY: Float
+    get() = downLocation.y
+
+    val currentLocationX: Float
+    get() = currentLocation.x
+
+    val currentLocationY: Float
+    get() = currentLocation.y
+
+
+    enum class State {
+        Available,
+        Began,
+        Changed,
+        Failed,
+        Cancelled,
+        Ended
+    }
 
     override fun onStateChanged(recognizer: GestureRecognizer) {
         TODO("Not yet implemented")
@@ -32,5 +59,35 @@ abstract class GestureRecognizer (context: Context): OnGestureRecognizerStateCha
     }
 
     protected abstract fun handelMessage(msg: Message)
+
+    var state: State? = null
+    protected set(value) {
+        val oldVal = field
+        val changed = this.state != value || value == State.Changed
+        field = value
+
+        if (changed){
+            stateChangeListener?.invoke(this, oldVal, value)
+            val iterator = alStateListeners.listIterator()
+            while (iterator.hasNext()){
+                iterator.next().onStateChanged(this)
+            }
+        }
+    }
+
+    var isEnabled: Boolean = true
+    set(value) {
+        if (field != value){
+            field = value
+            if (!value){
+                reset()
+            }
+        }
+    }
+
+
+    open fun reset(){
+        state = null
+    }
 
 }
